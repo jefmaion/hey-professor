a562c9fde002fe7ace2f5d9ae2539f1596c7b2f1
Jefferson Maion de Oliveira <you@example.com>
2024-04-29 17:37:25 -0300

    hey-11: handling n+1 issue

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -10,17 +9,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Question extends Model
 {
     use HasFactory;
-
-    public function likes(): Attribute
-    {
-        return new Attribute(fn () => $this->votes()->sum('like'));
-    }
-
-    public function unlikes(): Attribute
-    {
-        return new Attribute(fn () => $this->votes()->sum('unlike'));
-    }
-
     /**
      * Undocumented function
      *
--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -9,16 +9,17 @@
 
     <span>{{ $question->question }}</span>
     <div>
+      
         <x-form :action="route('question.like', $question)">
             <button class="flex items-start space-x-2  text-green-500 hover:text-green-400">
                 <x-icons.thumbs-up class="w-5 h-5 cursor-pointer " />
-                <span>{{ $question->likes }}</span>
+                <span>{{ $question->votes_sum_like ?: 0 }}</span>
             </button>
         </x-form>
         <x-form :action="route('question.unlike', $question)">
             <button class="flex items-start space-x-2  text-red-500 hover:text-red-400">
                 <x-icons.thumbs-down class="w-5 h-5  cursor-pointer " />
-                <span>{{ $question->unlikes }}</span>
+                <span>{{ $question->votes_sum_unlike ?: 0 }}</span>
             </button>
         </x-form>
     </div>
