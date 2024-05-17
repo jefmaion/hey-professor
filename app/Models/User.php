abe80b1cc7a5d437ddaebc6d37eddfcd56d36002
Jefferson Maion de Oliveira <you@example.com>
2024-05-17 18:22:14 -0300

    hey-14: it should make shure that only the person who has created then question can publish the question

--- app/Models/User.php
+++ app/Models/User.php
@@ -80,4 +80,9 @@ class User extends Authenticatable implements MustVerifyEmail
             ]
         );
     }
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class, 'created_by');
+    }
 }
--- tests/Feature/Question/PublishTest.php
+++ tests/Feature/Question/PublishTest.php
@@ -6,7 +6,7 @@ use function Pest\Laravel\{actingAs, put};
 
 it('should be able to publish a question', function () {
     $user     = User::factory()->create();
-    $question = Question::factory()->create(['draft' => true]);
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
 
     actingAs($user);
 
@@ -15,5 +15,22 @@ it('should be able to publish a question', function () {
     $question->refresh();
 
     expect($question)->draft->toBeFalse();
+
+});
+
+it('should make shure that only the person who has created then question can publish the question', function () {
+
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+
+    $question = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+
+    put(route('question.publish', $question))->assertForbidden();
+
+    actingAs($rightUser);
+
+    put(route('question.publish', $question))->assertRedirect();
 
 });
